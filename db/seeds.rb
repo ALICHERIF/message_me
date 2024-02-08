10a6dee5607a9e2710288d746072129b028224b6
ALICHERIF <adem.mohamed@hotmail.fr>
2024-02-08 16:47:24 +0100

    build User and Messages resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create(username:"mohamed",password: "password")
+User.create(username:"arya",password: "password")
+User.create(username:"fethi",password: "password")
+User.create(username:"houari",password: "password")
+User.create(username:"abdaka",password: "password")
