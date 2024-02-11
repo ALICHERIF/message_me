6cf6372620c3eaa5a59176be63a2f2798c47a6f2
ALICHERIF <adem.mohamed@hotmail.fr>
2024-02-11 18:34:06 +0100

    add autentication system create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,22 @@ class SessionsController < ApplicationController
   def new
   end
   def create
+  user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+  flash[:success] = "successful login "
+  redirect_to root_path
+    else
+      flash.now[:error] = "there was something wrong with your login details "
+      render 'new'
+      #binding.break
+    end
   end
+  def destroy
+  session[:user_id] = nil
+  #binding.break
+  flash[:success] = "successful logout "
+  redirect_to root_path
+end
+
 end
